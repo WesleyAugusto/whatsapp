e3ed7c6f3cfe7ff25d4198a017f6fc98818ed33d
WesleyAugusto <ranzerap@gmail.com>
2022-05-01 18:28:37 -0300

    Tela de Login

---
+++ lib/Login.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xff075E54) ),
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(padding:EdgeInsets.only(bottom: 32),
+                child: Image.asset(
+                  "imagens/logo.png",
+                  width: 200,
+                  height: 150,
+                ),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 8),
+                child: TextField(
+                  autofocus: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.fromLTRB(32, 20, 32, 20),
+                    hintText: "Email",
+                    filled: true,
+                    fillColor: Colors.white,
+
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40)
+                    )
+                  ),
+                ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 20, 32, 20),
+                      hintText: "Senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40)
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 10),
+                child: RaisedButton(
+                  child: Text("Entrar",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  color: Colors.green,
+                  padding: EdgeInsets.fromLTRB(162, 20, 162, 20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)
+                  ),
+                  onPressed: (){
+                    
+                  },
+                ),
+                ),
+                Center(
+                  child: GestureDetector(
+                    child: Text(
+                      "Não tem conta? Cadastre-se",
+                      style: TextStyle(
+                        color: Colors.white
+                      ),
+                    ),
+                    onTap: (){
+                    }
+                  ),
+                )
+
+              ],
+            ),
+          ),
+        ),
+      ) ,
+    );
+  }
+}
